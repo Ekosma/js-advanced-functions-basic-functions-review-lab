f8fba365b92a9ba45d72f37cabdcf53a60642547
Evelyn <evelyn.a.kosma@gmail.com>
2022-01-12 11:32:01 -0600

    commit tree e60e7e0145d35a7f49f74dcad7a7184868b1906b

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+function saturdayFun(activity='roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity='go to the office') {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(sym='*') {
+  return function (str) {
+    return `You are ${sym}${str}${sym}!`;
+  };
+}
+
+const Calculator = { 
+  add: (x,y) => {return x + y}, 
+  multiply: (x,y) => {return x * y},
+  subtract: (x,y) => {return x - y},
+  divide: (x,y) => {return x / y}
+}
+
+function actionApplyer(num, fun) {
+  return fun.reduce((prev, curr) => curr(prev), num)
+}
